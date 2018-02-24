58a1718aebd5820046febc5b92bb4f0c9fd7bac0
Rodrigo Dominguez <rorra@rorra.com.ar>
2018-02-24 08:28:34 -0300

    Setting up rake and customizing spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -99,4 +99,5 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  config.filter_gems_from_backtrace 'rack', 'rack-test', 'sequel', 'sinatra', 'rake'
 end
